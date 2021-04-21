2e5f0c6ace0599eb6e1f3041e0b7a9f5b10f09bd
Thomas Berghuis <thomas.berghuis@gmail.com>
2021-04-21 11:50:42 +1000

    .

--- lib/presentation/widgets/app_drawer.dart
+++ lib/presentation/widgets/app_drawer.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
+import '../../constants/enums.dart';
 
 class AppDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-
       child: ListView(
-
         padding: EdgeInsets.zero,
         children: <Widget>[
           DrawerHeader(
@@ -27,13 +26,27 @@ class AppDrawer extends StatelessWidget {
           ListTile(
             title: Text('lennox'),
             onTap: () {
-              
               // Navigator.pop(context);
               Navigator.popAndPushNamed(context, '/cam/lennox');
             },
           ),
+          for (var cam in Cam.values) DrawerEntry(cam),
         ],
       ),
+    );
+  }
+}
+
+class DrawerEntry extends StatelessWidget {
+  final Cam cam;
+  DrawerEntry(this.cam);
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(cam.name),
+      onTap: () {
+        Navigator.popAndPushNamed(context, '/cam', arguments: cam);
+      },
     );
   }
 }
